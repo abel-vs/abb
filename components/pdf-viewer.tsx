d0374319a605e6675a40352947197d7fb28cf3a4
abel <abel.van.steenweghen@gmail.com>
2023-12-10 11:37:38 +0200

    Waddup

--- components/pdf-viewer.tsx
+++ components/pdf-viewer.tsx
@@ -18,6 +18,25 @@ interface PdfViewerProps {
 const PdfViewer = ({ filePath, searchText = "" }: PdfViewerProps) => {
   const [numPages, setNumPages] = useState(null);
 
+  let matchCount = 0;
+  let prevMarkedElement = null;
+
+  function highlightPattern(text, pattern) {
+    return text.replace(pattern, (value) => {
+      matchCount++;
+      if (prevMarkedElement) {
+        prevMarkedElement.removeAttribute("id");
+      }
+      const mark = document.createElement("mark");
+      mark.id = "target-element";
+      mark.textContent = value;
+      if (matchCount === 1 || matchCount === 2) {
+        prevMarkedElement = mark;
+      }
+      return mark.outerHTML;
+    });
+  }
+
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
     scrollToElement("target-element");
@@ -32,12 +51,6 @@ const PdfViewer = ({ filePath, searchText = "" }: PdfViewerProps) => {
     }
   };
 
-  function highlightPattern(text, pattern) {
-    return text.replace(
-      pattern,
-      (value) => `<mark id="target-element">${value}</mark>`
-    );
-  }
   const textRenderer = (textItem) => {
     return highlightPattern(textItem.str, searchText);
   };
